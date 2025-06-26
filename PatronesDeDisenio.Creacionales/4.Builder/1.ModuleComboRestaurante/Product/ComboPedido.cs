01d3808a12761751eec7058228f6ab56d5353067
Ricardo Jara <rjjarag@hotmail.com>
2025-06-26 10:36:20 -0500

    Implementar ejemplo 1.ModuleComboRestaurante

---
+++ PatronesDeDisenio.Creacionales/4.Builder/1.ModuleComboRestaurante/Product/ComboPedido.cs
@@ -0,0 +1,21 @@
+﻿namespace PatronesDeDisenio.Creacionales._4.Builder._1.ModuleComboRestaurante.Product
+{
+    public class ComboPedido
+    {
+        public string? PlatoPrincipal { get; set; }
+        public string? Bebida { get; set; }
+        public string? Postre { get; set; }
+        public string? Juguete { get; set; }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Combo armado:");
+            Console.WriteLine($"- Plato Principal: {PlatoPrincipal}");
+            Console.WriteLine($"- Bebida: {Bebida}");
+            Console.WriteLine($"- Postre: {Postre}");
+
+            if (!string.IsNullOrEmpty(Juguete))
+                Console.WriteLine($"- Juguete: {Juguete}");
+        }
+    }
+}
