44373deafc16c36dbea423f49124a8bf746a7cc5
Ricardo Jara <rjjarag@hotmail.com>
2025-06-26 11:01:02 -0500

    Implementacion del ejemplo 3.ModuleContratoBuilder

---
+++ PatronesDeDisenio.Creacionales/4.Builder/3.ModuleContratoBuilder/Builders/ContratoAlquilerBuilder.cs
@@ -0,0 +1,26 @@
+﻿using PatronesDeDisenio.Creacionales._4.Builder._3.ModuleContratoBuilder.Director;
+
+namespace PatronesDeDisenio.Creacionales._4.Builder._3.ModuleContratoBuilder.Builders
+{
+    public class ContratoAlquilerBuilder : IContratoBuilder
+    {
+        private readonly Contrato _contrato = new();
+
+        public void ConstruirEncabezado()
+            => _contrato.Encabezado = "Contrato de Alquiler de Bien Inmueble";
+
+        public void ConstruirClausulas()
+            => _contrato.Clausulas = "Cláusulas sobre uso, duración, pago de renta y garantías.";
+
+        public void ConstruirCondicionesEspeciales()
+            => _contrato.CondicionesEspeciales = "Incluye cláusula de resolución anticipada con penalidad.";
+
+        public void ConstruirFirma()
+            => _contrato.Firma = "Firmado por arrendador y arrendatario con DNI.";
+
+        public void ConstruirAnexos()
+            => _contrato.Anexos = "Anexo de inventario de bienes y plano del inmueble.";
+
+        public Contrato ObtenerContrato() => _contrato;
+    }
+}
---
+++ PatronesDeDisenio.Creacionales/4.Builder/3.ModuleContratoBuilder/Director/ContratoDirector.cs
@@ -0,0 +1,16 @@
+﻿using PatronesDeDisenio.Creacionales._4.Builder._3.ModuleContratoBuilder.Builders;
+
+namespace PatronesDeDisenio.Creacionales._4.Builder._3.ModuleContratoBuilder.Director
+{
+    public class ContratoDirector
+    {
+        public void ConstruirContratoCompleto(IContratoBuilder builder)
+        {
+            builder.ConstruirEncabezado();
+            builder.ConstruirClausulas();
+            builder.ConstruirCondicionesEspeciales();
+            builder.ConstruirFirma();
+            builder.ConstruirAnexos();
+        }
+    }
+}
